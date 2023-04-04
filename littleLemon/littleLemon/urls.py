b3b0dc330a8d31e772ba6ba7e2df73d92b3d685c
Medymhd <113409513+Medymhd@users.noreply.github.com>
2023-04-04 13:11:34 +0300

    User API

--- littleLemon/littleLemon/urls.py
+++ littleLemon/littleLemon/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from restaurant import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserView)
+
 
 urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace = 'rest_framework')), #api-auth route is defined to let you use the browsable API feature of DRF
     path('admin/', admin.site.urls),
-    path('restaurant/', include('restaurant.urls')), #restaurant/
+    path('restaurant/', include('restaurant.urls')), 
+    path('users/', include(router.urls)),
 ]
+
+
--- littleLemon/restaurant/views.py
+++ littleLemon/restaurant/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
+from .serializers import UserSerializer
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html',{})
+
+
+class UserView(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
