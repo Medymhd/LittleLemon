29e9747daf4101feef2bc441cb03cd175b6a6874
Medymhd <113409513+Medymhd@users.noreply.github.com>
2023-04-09 11:55:33 +0300

    restaurant api submitting for testing

--- littleLemon/restaurant/urls.py
+++ littleLemon/restaurant/urls.py
@@ -3,9 +3,10 @@ from . import views
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='home'),
     path('menu-items/', views.MenuItemsView.as_view(), name = 'menu-items'),
     path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
     path('message/', views.msg),
-    path('api-token-auth/', obtain_auth_token)
+    path('api-token-auth/', obtain_auth_token),
+    path('about/', views.about, name = 'about')
 ]
--- littleLemon/restaurant/views.py
+++ littleLemon/restaurant/views.py
@@ -11,6 +11,9 @@ from rest_framework.response import Response
 def index(request):
     return render(request, 'index.html',{})
 
+def about(request):
+    return render(request, 'about.html')
+
 
 class UserViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
